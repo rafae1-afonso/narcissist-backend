059069a2b299dbc43a5ab90e912d546ffdd2020c
Rafael Afonso <122814044+rafae1-afonso@users.noreply.github.com>
2025-12-22 18:17:27 -0300

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import productRoutes from './routes/products.js';
 const app = express();
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+    res.setHeader('Access-Control-Allow-Origin', 'https://narcissist-frontend.onrender.com')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
